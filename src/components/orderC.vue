b71316cab9e0c7f2ea68919d6eaa7eea7e7cb085
zk8080 <zhangkang8080@163.com>
2018-05-28 17:40:17 +0800

    订单卡片

---
+++ src/components/orderC.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="page">
+    <div class="weui-form-preview">
+      <div class="weui-form-preview__hd">
+        <div class="weui-form-preview__item">
+          <div class="weui-form-preview__label">付款金额</div>
+          <div class="weui-form-preview__value_in-hd">¥{{orderData.price}}</div>
+        </div>
+      </div>
+      <div class="weui-form-preview__bd">
+        <div class="weui-form-preview__item">
+          <div class="weui-form-preview__label">商品</div>
+          <div class="weui-form-preview__value">{{orderData.title}}</div>
+        </div>
+        <div class="weui-form-preview__item">
+          <div class="weui-form-preview__label">数量</div>
+          <div class="weui-form-preview__value">{{orderData.orderNum}}</div>
+        </div>
+        <div class="weui-form-preview__item">
+          <div class="weui-form-preview__label">门店</div>
+          <div class="weui-form-preview__value">{{orderData.address}}</div>
+        </div>
+      </div>
+      <div class="weui-form-preview__ft">
+        <navigator url="" class="weui-form-preview__btn weui-form-preview__btn_primary" hover-class="weui-form-preview__btn_active">查看二维码</navigator>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['orderData']
+}
+</script>
+
+<style>
+
+</style>
+
+
---
+++ src/components/orderCard.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <div class="order-item">
+        <div class="header">
+            <text class="fl">订单编号：{{orderData.orderNo}}</text>
+            <text class="fr">{{orderData.status}}</text>  
+        </div>
+        <div class="order-detail">
+            <div class="order-img">
+                <!-- <img src="" alt=""> -->
+            </div>
+            <div class="order-title">
+                <text class="title">{{orderData.title}}</text>
+                <text class='totalprice'>
+                    <text class="num">{{orderData.orderNum}}张</text>
+                    <text class="price">¥{{orderData.price}}</text>
+                </text>
+            </div>
+        </div>
+        <!-- <div class="time">
+                <text>时间：2018-01-08 09:00</text>
+        </div> -->
+        <div class="address">
+                <text>地址：{{orderData.address}}</text>
+        </div>
+        <a href="">
+            <div class="ewm clearfix">
+            <text class="fr">查看二维码></text>
+            </div>
+        </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['orderData']
+}
+</script>
+
+<style>
+.order-item{
+  border: 1px solid #ccc;
+  border-radius: 6rpx;
+  box-shadow: 0 0 4rpx #888888;
+  padding: 10rpx 0;
+  margin-bottom: 20rpx;
+}
+.order-item .header{
+  font-size: 24rpx;
+  color: #ccc;
+  padding: 10rpx 20rpx;
+  display: flex;
+  justify-content: space-between;
+}
+.order-detail{
+  display: flex;
+  padding: 6rpx 20rpx;
+  position: relative;
+}
+.order-detail .order-img{
+  width: 180rpx;
+  height: 180rpx;
+}
+.order-detail .order-title{
+  flex: 1;
+  margin-left: 20rpx;
+  position: relative;
+  text-align: left;
+}
+.order-title .title{
+  display: block;
+  margin-top: 50rpx;
+  font-size: 32rpx;
+}
+.order-title .totalprice{
+  font-size: 32rpx;
+}
+.order-title .totalprice .num{
+  color: #707070;
+  margin-right: 10rpx;
+}
+.order-title .totalprice .price{
+  color: #d81e06;
+}
+.time{
+  padding: 0 20rpx;
+  color: #707070;
+  font-size: 28rpx;
+}
+.address{
+  padding: 0 20rpx;
+  color: #707070;
+  font-size: 28rpx;
+  margin-bottom: 10rpx;
+}
+.ewm{
+    text-align: right;
+  padding: 10rpx 20rpx;
+  border-top: 1px solid #ccc;
+  font-size: 32rpx;
+  color: #d81e06;
+}
+</style>
