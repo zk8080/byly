ca75fc9978f7cc80ce38d6f11b1be133a28b8720
zk8080 <zhangkang8080@163.com>
2018-05-30 22:36:01 +0800

    请求显示弹窗

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,19 +5,24 @@ const request = new Fly()
 request.config.baseURL='https://www.baoyanmall.cn';
 
 request.interceptors.request.use((request) => {
-  wx.showNavigationBarLoading()
+  // wx.showNavigationBarLoading()
+  wx.showLoading({
+    title: '加载中',
+  })
   request.headers['content-type']="application/x-www-form-urlencoded";
   return request
 })
 
 request.interceptors.response.use(
   (response, promise) => {
-    wx.hideNavigationBarLoading()
+    // wx.hideNavigationBarLoading()
+    wx.hideLoading();
     return promise.resolve(response.data)
     
   },
   (err, promise) => {
-    wx.hideNavigationBarLoading()
+    // wx.hideNavigationBarLoading()
+    wx.hideLoading();
     wx.showToast({
       title: err.message,
       icon: 'none'
